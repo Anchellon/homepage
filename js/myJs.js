cc067179c3d14417c88e1635eb9ddfafbef4b5ed
anchellon <anshulmathew@gmail.com>
2022-09-24 11:53:46 -0700

    Added about page, Links need to be added

---
+++ js/myJs.js
@@ -0,0 +1,9 @@
+let myNameSpace = function(){
+  function searchSite(){
+    let searchButton = document.getElementsByClassName('gsc-search-button gsc-search-button-v2');
+    searchButton[0].click();
+  }
+  return{
+    searchSite:searchSite,
+  }
+}();
