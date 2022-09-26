0b83c7fff7d7ad38c1f2e271eaf213ae231f90aa
anchellon <anshulmathew@gmail.com>
2022-09-26 14:18:33 -0700

    Added favicon

--- js/myJs.js
+++ js/myJs.js
@@ -1,9 +1,17 @@
-export const myNameSpace = function(){
-  function searchSite(){
-    let searchButton = document.getElementsByClassName('gsc-search-button gsc-search-button-v2');
+export const myNameSpace = (function () {
+  function searchSite() {
+    let searchButton = document.getElementsByClassName(
+      "gsc-search-button gsc-search-button-v2"
+    );
     searchButton[0].click();
   }
-  return{
-    searchSite:searchSite,
+  function feelingLucky() {
+    let searchButton = document.getElementsByClassName(
+      "gsc-search-button gsc-search-button-v2"
+    );
+    searchButton[0].click();
   }
-}();
+  return {
+    searchSite: searchSite,
+  };
+})();
