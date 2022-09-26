d8712c4ec53b517c0a05e90b8e1e1fe71dad5150
anchellon <anshulmathew@gmail.com>
2022-09-26 14:46:03 -0700

    added more js

--- js/myJs.js
+++ js/myJs.js
@@ -7,10 +7,75 @@ let myNameSpace = (function () {
     searchButton[0].click();
   }
   function feelingLucky() {
-    let searchButton = document.getElementsByClassName(
-      "gsc-search-button gsc-search-button-v2"
-    );
-    searchButton[0].click();
+    let topics = [
+      "Cold",
+      "Warm",
+      "Hot",
+      "Religion",
+      "Home",
+      "Family",
+      "Friends",
+      "Love",
+      "Hate",
+      "Political",
+      "Time",
+      "Literature",
+      "Pop Culture",
+      "Emotions",
+      "Current Events",
+      "How-To",
+      "Music",
+      "History",
+      "Education",
+      "Health",
+      "Money",
+      "Self",
+      "Celebration",
+      "Food",
+      "Holiday",
+      "Vacation",
+      "blank",
+      "Indoor",
+      "Outdoor",
+      "Task",
+      "Opinion",
+      "Controversy",
+      "Hobby",
+      "Mental",
+      "Health",
+      "Confidence",
+      "Nature",
+      "Vintage",
+      "Religion",
+      "Fiction",
+      "Non-Fiction",
+      "Law",
+      "Animals",
+      "People",
+      "Criticism",
+      "Career",
+      "Sports",
+      "Recreation",
+      "Children",
+      "Adults",
+      "Happy",
+      "Sad",
+      "Art",
+      "Technology",
+      "Favorite",
+      "Motivation",
+      "Inspiration",
+      "Winter",
+      "Fall",
+      "Spring",
+      "Summer",
+      "Memories",
+    ];
+    let i = Math.floor(Math.random() * topics.length);
+    let q = topics[i];
+    document.getElementById("btn_lucky").onclick = function () {
+      window.open("http://google.com/search?q=" + q);
+    };
   }
   return {
     searchSite: searchSite,
