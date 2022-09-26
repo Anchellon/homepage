1042f9e17965a2b8668abd5c52a15351db82d69d
anchellon <anshulmathew@gmail.com>
2022-09-26 13:22:47 -0700

    prettied it

--- js/myJs.js
+++ js/myJs.js
@@ -1,4 +1,4 @@
-let myNameSpace = function(){
+export const myNameSpace = function(){
   function searchSite(){
     let searchButton = document.getElementsByClassName('gsc-search-button gsc-search-button-v2');
     searchButton[0].click();
